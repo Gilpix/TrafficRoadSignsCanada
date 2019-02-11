f79d78d2ce11b2eb232089b6f24b61fe0ed45c1f
Gilpix <kuldeepsingh5005@gmail.com>
2019-02-11 11:46:58 -0500

    unused methods deleted from Home class

--- app/src/main/java/com/vid_lancer/trafficroadsignscanada/Home.java
+++ app/src/main/java/com/vid_lancer/trafficroadsignscanada/Home.java
@@ -16,8 +16,6 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-        
     }
 
 
